45c4bd936cc6f6fd5c9158019b4dc2732d6403ec
anitamaharana55 <anitamaharana55@.com>
2024-11-06 11:28:21 +0530

    updated

--- modules/container_registry/main.tf
+++ modules/container_registry/main.tf
@@ -1,11 +1,22 @@
-# resource "google_container_registry" "registry" {
-#   project  = var.project
-#   location = var.location
-# }
+resource "google_kms_key_ring" "keyring" {
+  name     = "test-keyring"
+  location = "global"
+}
+
+resource "google_kms_crypto_key" "example-key" {
+  name            = "test-crypto-key"
+  key_ring        = google_kms_key_ring.keyring.id
+  rotation_period = "7776000s"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
 resource "google_artifact_registry_repository" "my-repo" {
     project  = var.project
   location      = var.location
   repository_id = var.repository_id
   description   = var.description
   format        = var.format
+  kms_key_name = google_kms_crypto_key.example-key.id
 }
