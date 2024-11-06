42a9f00bf5c019486693b362fbbc6f7ef1ada831
anitamaharana55 <anitamaharana55@.com>
2024-11-06 11:00:20 +0530

    updated

--- modules/Project/main.tf
+++ modules/Project/main.tf
@@ -2,5 +2,6 @@ resource "google_project" "my_project" {
   name       = var.name
   project_id = var.project_id
   org_id     = var.org_id
+  auto_create_network = false
 count = var.enable_network ? 1 : 0
 }
