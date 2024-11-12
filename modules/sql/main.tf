138cf4c28f5c8722e38e0626bb37af6a62b21ab2
anitamaharana55 <anitamaharana55@gmail.com>
2024-11-12 16:12:50 +0530

    updatedjenkinsfile

--- modules/sql/main.tf
+++ modules/sql/main.tf
@@ -39,7 +39,7 @@ resource "google_sql_database_instance" "instance" {
   name             = var.sql_name
   region           = var.location
   database_version = "MYSQL_8_0" #var.database_version  #
-  #  depends_on = [google_service_networking_connection.default]
+  depends_on = [google_service_networking_connection.default]
   settings {
     tier = "db-f1-micro"
     
