75a05448779de884a57959efe537d3410d8f25a8
anitamaharana55 <anitamaharana55@gmail.com>
2024-11-12 13:59:19 +0530

    updatedjenkinsfile

--- variable.tf
+++ variable.tf
@@ -4,18 +4,18 @@ variable "project_config" {
 variable "VPC_config" {
 
 }
-variable "svc_config" {
+# variable "svc_config" {
 
-}
+# }
 variable "registry_config" {
 
 }
 variable "cloudrunsql_config" {
 
 }
-variable "secret_config" {
+# variable "secret_config" {
 
-}
+# }
 variable "sql_config" {
 
 }
